57ea216207c5ecf4129f99662f77d30e788f09e9
Emrandu1989 <emrandu1989@gmail.com>
2024-03-26 08:23:13 +0600

    404 page added

---
+++ src/components/ErrorPage.jsx
@@ -0,0 +1,14 @@
+
+
+const ErrorPage = () => {
+    return (
+        <div className="flex items-center justify-center h-screen">
+            <div className="text-center">
+                <h1 className="text-6xl font-bold mb-4">404 </h1>
+                <p className="text-gray-600">The page you are looking for is not available.</p>
+            </div>
+        </div>
+    );
+};
+
+export default ErrorPage;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "../layouts/Layout";
 import Home from "../pages/Home/Home";
 import BookDetails from "../components/BookDetails";
+import ErrorPage from "../components/ErrorPage";
 
 export const router = createBrowserRouter([
     {
       path: "/",
       element: <Layout />,
+      errorElement:<ErrorPage/>,
       children: [
            {
             path:'/',
